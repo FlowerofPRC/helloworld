0234842b04fb7579a8c172ee74d12e3f40cb1eab
syx <shouyixiang@163.com>
2016-07-14 23:59:36 +0800

    just test

---
+++ src/helloworld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by syx on 2016/7/14.
+ */
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("just test the github in my IntelliJ IDEA");
+    }
+}
